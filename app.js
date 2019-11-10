fad09536c2807f4ba06c64601b6c5a66934dd3c8
CheBlankenship <cnakamura336@gmail.com>
2019-11-10 03:15:23 -0600

    remove unnecessary npm lib

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var ibmdb = require('ibm_db');
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 
 
 
