953a1659d38330be3b1f85e1b450443c5de110ef
CheBlankenship <cnakamura336@gmail.com>
2019-11-14 20:29:03 -0600

    change pgdb credentials

--- postgres-config.js
+++ postgres-config.js
@@ -10,13 +10,25 @@
 // }
 
 
+// module.exports = {
+//   database:{
+//     db: "desk-life.postgres.database.azure.com",
+//     dbname: "postgres",
+//     port: 5432,
+//     passwd: "Grandmava-88",
+//     usrId: "cheblankenship88@desk-life",
+//     ssl: true
+//   }
+// }
+
+
 module.exports = {
   database:{
-    db: "desk-life.postgres.database.azure.com",
-    dbname: "postgres",
+    db: "ec2-174-129-253-53.compute-1.amazonaws.com",
+    dbname: "dbf2ajjal3qkjr",
     port: 5432,
-    passwd: "Grandmava-88",
-    usrId: "cheblankenship88@desk-life",
+    passwd: "2c671411877e7aca1e8ad19ea9095799309fb8c189da011c4c440028f204e1b8",
+    usrId: "pervbjdfwxtyuo",
     ssl: true
   }
 }
