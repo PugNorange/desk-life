9f4d98fcad6ec722b84f4ee6f698ca3ebb8f0db1
Che Blankenship <cheb@Ches-MacBook-Air.local>
2019-12-23 18:56:48 -0600

    update UI navbar. Connect DB to frontend.

--- public/js/data_chart_demo.js
+++ public/js/data_chart_demo.js
@@ -1,39 +1,52 @@
+console.log("DATA >>> ", dataForChart);
+// Create list of time labels for x-axis
+// Timezone issue needs to be solved later.
+var xAxisTimeLabel = ["00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00",
+"08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00",
+"16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00",];
+// function getXAxisLabels(dataForChart) {
+//     for (var i = 0; i < dataForChart.length; i++) {
+//         console.log("SEE >> ", dataForChart[i]["start_time"]);
+//         var modifyTimeStamp = dataForChart[i]["start_time"].split("T");
+//         var reformat = modifyTimeStamp[1].split(":");
+//         var xAxisTimes = reformat[0]+":"+reformat[1];
+//         xAxisTimeLabel[i] = xAxisTimes;
+//         console.log("CHECK time >> ", xAxisTimes);
+//     }
+// };
 
-// var total_money= ['100','150','120','460','700','660','890','900','890','950','1300','1123']
-//
-// /// Eye blink data
-// var members = ['40','60','100','95','180','190','196','220','211','205','215','240']
 
 
-/// Distance between face and PC (inches) //
-var distance = ['0','0','0','0','0','0','0','0','15','16',
-'13','11','12','0','9','7','5','0','0','0',
-'0','0','0','0']
 
-/// Eye blink data
-var eye_blink = ['0','0','0','0','0','0',
-    '0','0','11','15','9','5','4',
-    '0','11','5','0','0','0','0','0','0'
-]
+var faceDistanceData = [];
+function getFaceDistanceData(dataForChart) {
+    for (var i = 0; i < dataForChart.length; i++) {
+        faceDistanceData[i] = dataForChart[i]["face_distance"];
+    }
+};
 
-var time_label = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00',
-'07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00',
-'16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00']
+var eyeBlinkData = [];
+function getEyeBlinkData(dataForChart) {
+    for (var i = 0; i < dataForChart.length; i++) {
+        eyeBlinkData[i] = dataForChart[i]["eye_blink_count"];
+    }
+}
+
 
 var barChartData = {
-    labels: time_label,
+    labels: xAxisTimeLabel,
     datasets: [
         {
             type: 'line',
             label: 'Distance between face and PC (inches)',
-            data: distance,
+            data: faceDistanceData,
             borderColor : "rgba(254,97,132,0.8)",
             backgroundColor : "rgba(254,97,132,0.5)",
         },
         {
             type:'bar',
             label: 'Number of eye blinks per minute',
-            data: eye_blink,
+            data: eyeBlinkData,
             borderColor : "rgba(54,164,235,0.8)",
             backgroundColor : "rgba(54,164,235,0.5)",
         },
@@ -44,6 +57,10 @@ var barChartData = {
 window.onload = function() {
     ctx = document.getElementById("canvas").getContext("2d");
 
+    // Get data for the chart
+    // getXAxisLabels(dataForChart); // x-axis label
+    getFaceDistanceData(dataForChart); // face distance data
+    getEyeBlinkData(dataForChart); // Eye blink data
     window.myBar = new Chart(ctx, {
         type: 'bar', // ここは bar にする必要があります
         data: barChartData,
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var pg = require('pg');
 var fs = require('fs');
 var postgres_config = require("./../postgres-config").database;
+var jstz = require('jstimezonedetect');
+var tz = jstz.determine(); //timezone Detector
+
 
 var config = new pg.Client({
   host: postgres_config.db,
@@ -17,9 +20,37 @@ var config = new pg.Client({
 const pgClient = new pg.Client(config);
 pgClient.connect(err => {if (err) throw err;});
 
+
+// var xAxisTimeLabel = [];
+// function getXAxisLabels(dataForChart) {
+//     for (var i = 0; i < dataForChart.length; i++) {
+//         console.log("SEE >> ", dataForChart[i]["start_time"]);
+//         var modifyTimeStamp = dataForChart[i]["start_time"].split("T");
+//         var reformat = modifyTimeStamp[1].split(":");
+//         var xAxisTimes = reformat[0]+":"+reformat[1];
+//         xAxisTimeLabel[i] = xAxisTimes;
+//         console.log("CHECK time >> ", xAxisTimes);
+//     }
+// };
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index.ejs');
+    const query = `SELECT * FROM HEALTHINFO WHERE user_id=1 ORDER BY start_time;`;
+    pgClient
+        .query(query)
+        .then((data, err) => {
+            console.log("Data >>> ", data.rows[0].start_time);
+            console.log('Query successfully!', data.rows);
+
+            var ch_data = JSON.stringify(data.rows);
+            res.render('index.ejs', {chart_data: ch_data});
+        })
+        .catch((err) => {
+            console.log(err)
+            pgClient.end();
+    });
+    // res.render('index.ejs');
 });
 
 router.get('/face', function(req, res, next) {
@@ -45,7 +76,7 @@ router.get('/data_chart_demo', function(req, res, next) {
         .catch((err) => {
             console.log(err)
             pgClient.end();
-        });
+    });
 });
 
 router.get('/login', function(req, res, next){
