87939b58db20b4dfc5af26d8904f7665a97a43f2
Che Blankenship <cheb@Ches-MacBook-Air.local>
2019-12-25 04:29:21 -0600

    google authentication is integrated into the server

---
+++ public/js/googleLogin.js
@@ -0,0 +1,56 @@
+//
+// Google Signin / Login function and design
+//
+// first time sign In //
+function onSignIn(googleUser) {
+    console.log("Clicked google signin btn");
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    // console.log("ID Token   : " + id_token);
+
+    var profile = googleUser.getBasicProfile();
+
+    // auth2 is initialized with gapi.auth2.init() and a user is signed in.
+    // console.log('ID         : ' + profile.getId());
+    // console.log('Full Name  : ' + profile.getName());
+    // console.log('Given Name : ' + profile.getGivenName());
+    // console.log('Family Name: ' + profile.getFamilyName());
+    // console.log('Image URL  : ' + profile.getImageUrl());
+    // console.log('Email      : ' + profile.getEmail());
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/tokensignin');
+    // xhr.open('POST', 'http://localhost:3000/');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        const response = xhr.responseText
+        console.log('Server returned: ' + response);
+    };
+    xhr.send('idtoken=' + id_token);
+
+}
+
+// for sign out //
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+  console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -16,21 +16,23 @@ var config = new pg.Client({
 })
 const pgClient = new pg.Client(config);
 pgClient.connect(err => {if (err) throw err;});
-// Authentication and client for Google Signin
+// clientID for Google Signin
+// const CLIENT_ID = "199579559715-n165q0hs5n5fc5r1vhk72t29n4sag7i8"
 const {OAuth2Client} = require('google-auth-library');
-const CLIENT_ID = "199579559715-n165q0hs5n5fc5r1vhk72t29n4sag7i8"
-const client = new OAuth2Client(CLIENT_ID);
-
-
+var CLIENT_ID = require("./../ggl_cli_id").clientIdUrl;
 /* GET home page. */
 router.get('/', function(req, res, next) {
     const query = `SELECT * FROM HEALTHINFO WHERE user_id=2 ORDER BY start_time;`;
     pgClient
         .query(query)
         .then((data, err) => {
-            console.log('Query successfully!', data.rows);
+            // console.log('Query successfully!', data.rows);
             var ch_data = JSON.stringify(data.rows);
-            res.render('index.ejs', {chart_data: ch_data});
+            res.render('index.ejs', {
+                chart_data: ch_data,
+                title: "home",
+                clientIdUrl: CLIENT_ID
+            });
         })
         .catch((err) => {
             console.log(err)
@@ -39,75 +41,180 @@ router.get('/', function(req, res, next) {
     // res.render('index.ejs');
 });
 
-router.post('/', function(req, res, next){
-    console.log("CHECK IF GOOGLE LOGIN WORKIGN>>>>>");
-    async function verify() {
-      const ticket = await client.verifyIdToken({
-          idToken: token,
-          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-          // Or, if multiple clients access the backend:
-          //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-      });
-      const payload = ticket.getPayload();
-      const userid = payload['sub'];
-      // If request specified a G Suite domain:
-      //const domain = payload['hd'];
-    }
-    verify().catch(console.error);
-})
+// const {OAuth2Client} = require('google-auth-library');
 
-router.get('/face', function(req, res, next) {
-  res.render('face.ejs');
+
+router.post('/tokensignin', async (req, res, next) => {
+    try {
+        const client = new OAuth2Client(CLIENT_ID);
+        const idToken = String(req.body.idtoken);
+        // console.log("TOKEN >>> ", idToken);
+        async function verify() {
+            const ticket = await client.verifyIdToken({
+                idToken: idToken,
+                audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+                // Or, if multiple clients access the backend:
+                //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+            });
+            // If token is valid & email is verified, get the user information.
+            const payload   = ticket.getPayload(); // Account information
+            if (payload.email_verified === true) {
+                const userid    = payload['sub']; // constant usrid
+                const email     = payload['email']; // email
+                const username  = payload['name']; // user name
+                //console.log("usrID: ", userid, " email: ", email, " user name: ", username);
+                const query = `SELECT * FROM USERS WHERE gmail_token_id='${userid}';`;
+                pgClient
+                    .query(query)
+                    .then((data, err) => {
+                        // console.log('Query successfully!', data.rows);
+                        var usr_data = JSON.stringify(data.rows);
+                        if (usr_data.length > 0) {
+                            console.log("USER DATA >> ", usr_data[0]);
+                            // If username or email is update, change it in db also.
+                            if ((usr_data[0].user_name != username)||(usr_data[0].email != email)) {
+                                var updateUserInfoQuery = `UPDATE USERS SET email = '${email}', user_name='${username}' WHERE gmail_token_id = '${userid}';`;
+                                pgClient
+                                    .query(updateUserInfoQuery)
+                                    .then((data, err) => {
+                                        console.log("Data ?? ", data);
+                                        // console.log('Query successfully!', data.rows);
+                                        // res.render('index.ejs', {
+                                        //     chart_data: ch_data,
+                                        //     title: "home",
+                                        //     clientIdUrl: CLIENT_ID
+                                        // });
+                                        console.log("update success!!");
+                                        var userData = JSON.stringify({username: username, email: email});
+                                        res.send(userData)
+                                    })
+                                    .catch((err) => {
+                                        console.log(err)
+                                        pgClient.end();
+                                });
+
+                            }
+
+                            //res.send("check if its working");
+
+
+                        }
+                        else {
+                            console.log("No user table update");
+                            var userData = JSON.stringify({username: username, email: email});
+                            res.send(userData)
+                        }
+                        // res.render('index.ejs', {
+                        //     chart_data: ch_data,
+                        //     title: "home",
+                        //     clientIdUrl: gglClientIdUrl
+                        // });
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        pgClient.end();
+                });
+            }
+
+        }
+        verify().catch(console.error); // If token is invalid.
+
+    // write query here
+
+    // const query = `SELECT * FROM USERS WHERE gmail_token_id='faf824e5-5b81-4224-84b6-98687c0af9fe';`;
+    // const query_two = `INSERT INTO Users(user_name, passwd, email, gmail_token_id)
+// VALUES('demouser1', 'testtest1', 'test1@gmail.com', 'cd43de77-b669-4567-9428-b48e93956b78');`;
+
+
+
+    // res.send("Hey success!!")
+
+
+  } catch (e) {
+    next(e);
+  }
 });
+
+
+// router.post('/', function(req, res, next){
+//     const token = req.body.idtoken;
+//     const CLIENT_ID = "199579559715-n165q0hs5n5fc5r1vhk72t29n4sag7i8.apps.googleusercontent.com";
+//     const client = new OAuth2Client(CLIENT_ID);
+//
+//     // console.log("CHECK IF GOOGLE LOGIN WORKIGN>>>>>");
+//     // async function verify() {
+//     //   const ticket = await client.verifyIdToken({
+//     //       idToken: token,
+//     //       audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+//     //       // Or, if multiple clients access the backend:
+//     //       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+//     //   });
+//       // const payload = ticket.getPayload();
+//       // const userid = payload['sub'];
+//       // const user = {};
+//       // user.email = payload['email'];
+//       // user.picture = payload['picture'];
+//       // user = JSON.stringify(user);
+//       // res.render('index.ejs', {user_data: user});
+//       // res.send(user);
+//       // If request specified a G Suite domain:
+//       //const domain = payload['hd'];
+//     }
+//     verify().catch(console.error);
+// })
+
+// router.get('/face', function(req, res, next) {
+//   res.render('face.ejs');
+// });
 
 
 router.get('/instructions', function(req, res, next) {
     res.render('instructions.ejs');
 });
 
-router.get('/data_chart_demo', function(req, res, next) {
-    console.log("check point");
+// router.get('/data_chart_demo', function(req, res, next) {
+//     console.log("check point");
+//
+//     const query = `SELECT * FROM HEALTHINFO WHERE user_id=1;`;
+//     pgClient
+//         .query(query)
+//         .then((data, err) => {
+//             // console.log('Query successfully!', data.rows);
+//             var ch_data = JSON.stringify(data.rows);
+//             res.render('data_chart_demo', {chart_data: ch_data});
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//             pgClient.end();
+//     });
+// });
 
-    const query = `SELECT * FROM HEALTHINFO WHERE user_id=1;`;
-    pgClient
-        .query(query)
-        .then((data, err) => {
-            // console.log('Query successfully!', data.rows);
-            var ch_data = JSON.stringify(data.rows);
-            res.render('data_chart_demo', {chart_data: ch_data});
-        })
-        .catch((err) => {
-            console.log(err)
-            pgClient.end();
-    });
-});
+// router.get('/login', function(req, res, next){
+//     res.render('login.ejs');
+// });
 
-router.get('/login', function(req, res, next){
-    res.render('login.ejs');
-});
-
-router.post('/login', function(req, res, next){
-    var email = req.body.email;
-    var password = req.body.password;
-    // Connect to DB and check if it matches.
-    // pg.connect(link, function (err,connect) {
-    //     if (err) return console.log(err);
-
-        var q = `select * from Users WHERE email='${email}' AND passwd='${password}'`;
-        console.log("check >> ", q);
-        pgClient.query(q, function (err, data) {
-            if (err){
-                console.log("ERROR >> ", err);
-                pgClient.end();
-            }
-            else {
-                console.log("Success !! ", data);
-                res.render('index.ejs');
-            }
-        });
-    // });
-
-});
+// router.post('/login', function(req, res, next){
+//     var email = req.body.email;
+//     var password = req.body.password;
+//     // Connect to DB and check if it matches.
+//     // pg.connect(link, function (err,connect) {
+//     //     if (err) return console.log(err);
+//
+//         var q = `select * from Users WHERE email='${email}' AND passwd='${password}'`;
+//         console.log("check >> ", q);
+//         pgClient.query(q, function (err, data) {
+//             if (err){
+//                 console.log("ERROR >> ", err);
+//                 pgClient.end();
+//             }
+//             else {
+//                 console.log("Success !! ", data);
+//                 res.render('index.ejs');
+//             }
+//         });
+//     // });
+//
+// });
 
 
 module.exports = router;
