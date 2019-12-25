645a714fafe1e180001fbc8f1ad0f01e52556a9d
Che Blankenship <cheb@Ches-MacBook-Air.local>
2019-12-25 05:11:26 -0600

    change the port env

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var pg = require('pg');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set( 'port', ( process.env.PORT || 3000 ));
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -23,6 +24,6 @@ app.use('/', indexRouter);
 
 
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Listening on 3000');
+app.listen(app.get('port'), function() {
+  console.log('Listening on ', app.get('port'));
 });
